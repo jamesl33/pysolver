10512eae121b2287b1fd41516d26f46723e43d75
James Lee <jamesl33info@gmail.com>
2019-05-15 14:21:11 +0100

    Update copyright, LICENSE and README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-pysolver is a Rubik's cube solver which utilizes the CFOP algorithm.
---------------------------------------------------------------------
+pysolver is an *unfinished* Rubik's cube solver which will utilize the CFOP algorithm
+-------------------------------------------------------------------------------------
+pysolver is a project which I began before starting my second year at Coventry
+University (hence the reason it is currently unfinished). I started this
+project after realising that this problem could be more *easily* tackled using
+rotation vectors (which I learned about during my [pycube](https://github.com/jamesl33/pycube) project).
+
+License
+-------
+Copyright (C) 2019 James Lee <jamesl33info@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- pysolver/__init__.py
+++ pysolver/__init__.py
@@ -2,11 +2,11 @@
 """
 This file is part of pysolver.
 
-Copyright (C) 2018, James Lee <jamesl33info@gmail.com>.
+Copyright (C) 2019, James Lee <jamesl33info@gmail.com>.
 
-This program is free software; you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -14,13 +14,12 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __title__ = 'pysolver'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __author__ = 'James Lee'
-__license__ = 'GPL-2.0-or-later'
-__copyright__ = 'Copyright (C) 2018, James Lee'
+__license__ = 'GPL-3.0-or-later'
+__copyright__ = 'Copyright (C) 2019, James Lee'
